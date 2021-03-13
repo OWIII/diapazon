35d2b3d26b6ae47e39c5859a743d3b7f2bfc79ea
Lusnikov Artem <Lusnikov.A@digdes.com>
2021-03-14 00:03:17 +0300

    Add server

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,3 +1,3 @@
 export const ALL_IMAGES = "/api/contents?search=parent_id:94;is_active:1&offset=12&orderBy=ord&sortedBy=asc&searchJoin=and";
 export const ALL_IMAGES_PROD = "https://api.slav-nayka.ru/api/contents?search=parent_id:94;is_active:1&offset=12&orderBy=ord&sortedBy=asc&searchJoin=and";
-export const PROXY = "https://cors-anywhere.herokuapp.com/"
+export const PROXY = "https://diapazone.herokuapp.com/"
